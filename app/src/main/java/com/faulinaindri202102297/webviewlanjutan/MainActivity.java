56448c71f81f09c0a7e06c084cfba6b9c9e34f25
faulinaindri <113485041+faulina1308@users.noreply.github.com>
2023-04-06 10:31:59 +0700

    Initial commit

---
+++ app/src/main/java/com/faulinaindri202102297/webviewlanjutan/MainActivity.java
@@ -0,0 +1,43 @@
+package com.faulinaindri202102297.webviewlanjutan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity
+{
+    private WebView _webView1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        _webView1 = (WebView) findViewById(R.id.webView1);
+
+        WebViewClient webViewClient = new WebViewClient();
+        _webView1.setWebViewClient(webViewClient);
+
+        WebChromeClient webChromeClient = new WebChromeClient();
+        _webView1.setWebChromeClient(webChromeClient);
+
+        WebSettings webSettings = _webView1.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+
+        WebAppInterface webAppInterface = new WebAppInterface(this, MainActivity.this );
+        _webView1.addJavascriptInterface(webAppInterface, "Android" );
+
+        String url = "https://stmikpontinak.net/011100862/webview_lanjutan.html";
+        _webView1.loadUrl(url);
+    }
+}
